b8e7c226e59b0bd3783ec0bec39a77c1ff951e7a
Pedro <pedropadilha15.pp@gmail.com>
2023-07-16 21:12:55 -0300

    bug de file static resolvido

--- toposcriativos/settings.py
+++ toposcriativos/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Essa função concatena duas urls
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
